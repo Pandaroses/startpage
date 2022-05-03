fa4d6b22618c04ebc96cae33e922f7b208d9ee54
Pandaroses <georgiytnimov@gmail.com>
2022-05-03 13:57:49 +0100

    fart

--- src/js/terminal.js
+++ src/js/terminal.js
@@ -1,12 +1,14 @@
+
+
 // Terminal config
 config = {
-    shellPrompt: "$ "
+    shellPrompt: " ➜ "
 }
 
 document.getElementById("input_title").innerText = config.shellPrompt;
 document.getElementById('input_source').addEventListener('keyup', submit_command);
 document.getElementById('input_source').addEventListener('keyup', getLastCommand);
-
+document.getElementById('input_source').addEventListener('keydown', tab_complete);
 var current_block;
 
 function new_block() {
@@ -16,16 +18,14 @@ function new_block() {
 }
 
 function block_log(message) {
-    current_block.innerHTML = "<p>" + message + "</p>";
     new_block();
+    current_block.innerHTML = "<p>" + message + "</p>";
 }
 
-function submit_command() {
-    if (!(event.keyCode === 13)) return;
+function submit_command(e) {
+    if (!(e.keyCode === 13)) return;
     var input = document.getElementById("input_source").value;
     document.getElementById("input_source").value = "";
-
-    new_block();
 
     command = input.split(" ")[0];
     args = input.replace(command, "")
@@ -35,13 +35,83 @@ function submit_command() {
         window[command](args);
         lastCommand = command + args;
     } else if (command != "") {
-        block_log("command not found : " + command);
+        block_log("gsh: command not found : " + command);
     }
 }
 
-function getLastCommand() {
-    if (!(event.keyCode === 38)) return;
+function getLastCommand(e) {
+    if (!(e.keyCode === 38)) return;
     if (lastCommand) {
         document.getElementById("input_source").value = lastCommand;
     }
 }
+
+function tab_complete(e) {
+    if (!(e.keyCode === 9)) return;
+    e.preventDefault();
+    block_log(Object.values(cmds).filter(c => c.name.startsWith(document.getElementById("input_source").value)).map(c => c.name).join(" "));
+    //  var fart = cmds.filter(c => c.name.startsWith(document.getElementById("input_source").value)).map(c => c.name);
+    
+}   
+
+/// COMMANDS
+
+function google(args) {
+    if (args != "") {
+        search = args.replace(" ", "+")
+        window.open("https://www.google.com/search?q=" + search);
+    } else {
+        window.open("https://www.google.com");
+    }
+}
+
+function youtube(args){
+    if (args != "") {
+        search = args.replace(" ", "+")
+        window.open("https://www.youtube.com/results?search_query=" + search);
+    } else {
+        window.open("https://www.youtube.com");
+    }
+}
+
+function clock(args) {
+    var today = new Date();
+    block_log(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+}
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+function day(args) {
+    block_log(days[ now.getDay() ]);
+
+}
+
+function clear(args) {
+    document.getElementById('wrapper').innerHTML = "";
+}
+
+function open(args){
+    window.open("https://www."+search);
+    
+}
+
+//ls 
+function cmd(args){
+    block_log(Object.entries(cmds).filter(([k,v]) => k==v.name).map(c => c.name).join(" "));
+}
+
+var cmds = {
+    google,
+    g:google,
+    youtube,
+    yt:youtube,
+    clock,
+    time:clock,
+    day,
+    open,
+    o:open,
+    cmd
+};
+
+function echo(args){
+    block_log(args);
+};
